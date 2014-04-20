25b58192513549db6f5917a35191d7f67861b039
Robert Luke <robert.g.luke@gmail.com>
2014-04-20 17:53:58 +0200

    Updated example

--- examples/example.jl
+++ examples/example.jl
@@ -8,19 +8,19 @@ dats, evtTab, trigChan, sysCodeChan = readBdf("../data/Example_40Hz_SWN_70dB_R.b
 bdfInfo = readBdfHeader("../data/Example_40Hz_SWN_70dB_R.bdf");
 ChanName = bdfInfo["chanLabels"][ChannelToAnalyse]
 
-dats = proc_hp(dats)
+dats = proc_hp(dats, verbose=true)
 
 println("Referencing to $(bdfInfo["chanLabels"][48])")
-dats = proc_rereference(dats, 48)
+dats = proc_rereference(dats, 48, verbose=true)
 
 singleChan = convert(Array{Float64}, vec(dats[ChannelToAnalyse,8192*2:end]))
 f = plot_timeseries(singleChan, 8192, titletext=ChanName)
 file(f, "Eg1-RawData.png", width=1200, height=600)
 
-epochs = proc_epochs(dats, evtTab)
+epochs = proc_epochs(dats, evtTab, verbose=true)
 epochs = epochs[:,3:end,:]
 
-sweeps = proc_sweeps(epochs)
+sweeps = proc_sweeps(epochs, verbose=true)
 
 sweeps = squeeze(mean(sweeps,2),2)
 
