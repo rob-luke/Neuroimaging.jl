12c2212b798a9bf23d497f9e9ef5243dd3efb0f2
Robert Luke <robert.g.luke@gmail.com>
2014-04-20 17:51:50 +0200

    Added progress meter

--- src/processing.jl
+++ src/processing.jl
@@ -1,13 +1,21 @@
 using DataFrames
 using DSP
+using ProgressMeter
 
 
-function proc_hp(signals::Array; cutOff::Number=2, order::Int=3, fs::Int=8192)
+function proc_hp(signals::Array; cutOff::Number=2,
+                   order::Int=3, fs::Int=8192, verbose::Bool=false)
 
     signals = convert(Array{Float64}, signals)
 
     Wn = cutOff/(fs/2)
     f = digitalfilter(Highpass(Wn), Butterworth(order))
+
+    if verbose
+        println("Highpass filtering $(size(signals)[1]) channels")
+        println("  Pass band > $(cutOff) Hz = $(Wn)")
+        p = Progress(size(signals)[1], 1, "  Filtering...", 50)
+    end
 
     chan = 1
     while chan <= size(signals)[1]
@@ -15,6 +23,7 @@ function proc_hp(signals::Array; cutOff::Number=2, order::Int=3, fs::Int=8192)
         signals[chan,:] = fliplr(signals[chan,:])
         signals[chan,:] = filt(f, vec(signals[chan,:]))
         signals[chan,:] = fliplr(signals[chan,:])
+        if verbose; next!(p); end
         chan += 1
     end
 
@@ -22,11 +31,17 @@ function proc_hp(signals::Array; cutOff::Number=2, order::Int=3, fs::Int=8192)
 end
 
 
-function proc_rereference(signals::Array, refChan::Int)
+function proc_rereference(signals::Array, refChan::Int; verbose::Bool=false)
+
+    if verbose
+        println("Re referencing $(size(signals)[1]) channels")
+        p = Progress(size(signals)[1], 1, "  Rerefing... ", 50)
+    end
 
     chan = 1
     while chan <= size(signals)[1]
         signals[chan,:] = signals[chan,:] - signals[refChan,:]
+        if verbose; next!(p); end
         chan += 1
     end
 
@@ -44,14 +59,15 @@ function proc_epochs(dats::Array, evtTab::Dict; verbose::Bool=false)
     numEpochs = size(epochIndex)[1] - 1
     lenEpochs = minimum(diff(epochIndex[:Index]))
     numChans  = size(dats)[1]
+    epochs = zeros(Float64, (lenEpochs, numEpochs, numChans))
 
     if verbose
-        println("Epoch length is $(lenEpochs)")
-        println("Number of epochs is $(numEpochs)")
-        println("Number of channels is $(numChans)")
+        println("Generating epochs for $(numChans) channels")
+        println("  Epoch length is $(lenEpochs)")
+        println("  Number of epochs is $(numEpochs)")
+        println("  Number of channels is $(numChans)")
+        p = Progress(numChans, 1, "  Epoching...", 50)
     end
-
-    epochs = zeros(Float64, (lenEpochs, numEpochs, numChans))
 
     chan = 1
     while chan <= numChans
@@ -65,6 +81,7 @@ function proc_epochs(dats::Array, evtTab::Dict; verbose::Bool=false)
 
             epoch += 1
         end
+        if verbose; next!(p); end
         chan += 1
     end
 
@@ -80,12 +97,13 @@ function proc_sweeps(epochs::Array; epochsPerSweep::Int=4, verbose::Bool=false)
 
     sweepLen = epochsLen * epochsPerSweep
     sweepNum = floor(epochsNum / epochsPerSweep)
-
     sweeps = zeros(Float64, (sweepLen, int(sweepNum), chansNum))
 
     if verbose
-        println("From $(epochsNum) epochs of length $(epochsLen)")
-        println("Creating $(sweepNum) sweeps of length $(sweepLen)")
+        println("Generating $(sweepNum) sweeps")
+        println("  Frrom $(epochsNum) epochs of length $(epochsLen)")
+        println("  Creating $(sweepNum) sweeps of length $(sweepLen)")
+        p = Progress(int(sweepNum), 1, "  Sweeps...  ", 50)
     end
 
     sweep = 1
@@ -97,6 +115,7 @@ function proc_sweeps(epochs::Array; epochsPerSweep::Int=4, verbose::Bool=false)
         sweeps[:,sweep,:] = reshape(epochs[:,sweepStart:sweepStop,:],
                                                 (sweepLen, 1, chansNum))
 
+        if verbose; next!(p); end
         sweep += 1
     end
 
