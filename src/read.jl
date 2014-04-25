1f8c67dea4f15dff2b203b9c50a2ed62ff010656
Robert Luke <robert.g.luke@gmail.com>
2014-04-25 15:59:54 +0200

    Read all BSA information

--- src/read.jl
+++ src/read.jl
@@ -1,12 +1,18 @@
 using Winston
 using DataFrames
 
-type BSA
+type Dipoles
     version::String
     coord_system::String
     xloc::Array
     yloc::Array
     zloc::Array
+    xori::Array
+    yori::Array
+    zori::Array
+    color::Array
+    state::Array
+    size::Array
 end
 
 function read_bsa(fname::String; verbose::Bool=true)
@@ -26,9 +32,16 @@ function read_bsa(fname::String; verbose::Bool=true)
     m = match(regexp, title_line)
 
     # Create variables to fill
-    xloc = Float64[]
-    yloc = Float64[]
-    zloc = Float64[]
+    xloc  = Float64[]
+    yloc  = Float64[]
+    zloc  = Float64[]
+    xori  = Float64[]
+    yori  = Float64[]
+    zori  = Float64[]
+    color = Float64[]
+    state = Float64[]
+    size  = Float64[]
+
 
     line_line  = readline(fid)
 
@@ -36,21 +49,28 @@ function read_bsa(fname::String; verbose::Bool=true)
         println("File    = $fname")
         println("Version = $version")
         println("Coords  = $coordinate_system")
+        println("$title_line")
     end
 
     while ~eof(fid)
         d = readline(fid)
         dm = match(regexp, d)
 
-        push!(xloc, float(dm.captures[2]))
-        push!(yloc, float(dm.captures[3]))
-        push!(zloc, float(dm.captures[4]))
+        push!(xloc,  float (dm.captures[2]))
+        push!(yloc,  float (dm.captures[3]))
+        push!(zloc,  float (dm.captures[4]))
+        push!(xori,  float (dm.captures[5]))
+        push!(yori,  float (dm.captures[6]))
+        push!(zori,  float (dm.captures[7]))
+        push!(color, float (dm.captures[8]))
+        push!(state, float (dm.captures[9]))
+        push!(size,  float (dm.captures[10]))
 
     end
 
     close(fid)
 
-    bsa = BSA(version, coordinate_system, xloc, yloc, zloc)
+    bsa = Dipoles(version, coordinate_system, xloc, yloc, zloc, xori, yori, zori, color, state, size)
 
     return bsa
 
