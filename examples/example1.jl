bc5d4b96d1fb5c08d62f5becd48844abd6e53b76
Robert Luke <robert.g.luke@gmail.com>
2014-05-22 21:03:40 +0200

    Updated example to use EEG type

--- examples/example1.jl
+++ examples/example1.jl
@@ -1,30 +1,34 @@
-using JBDF
 using EEGjl
 using Winston
 
+using EEGjl
+using Winston
 
-# Import data
 fname = "../data/Example-40Hz.bdf"
-dats, evtTab, trigChan, sysCodeChan = readBdf(fname)
-bdfInfo = readBdfHeader(fname)
-bdfInfo["chanLabels"] = channelNames_biosemi_1020(bdfInfo["chanLabels"], verbose=false)
 
-# What channel to look at
-ChannelToAnalyse = 52;
-ChanName = bdfInfo["chanLabels"][ChannelToAnalyse]
+s = read_EEG(fname, verbose=true)
 
-dats = proc_hp(dats, verbose=true)
+s = proc_hp(s, verbose=true)
 
-dats = proc_reference(dats, "Cz", bdfInfo["chanLabels"], verbose=true)
+s = proc_reference(s, "average", verbose=true)
 
-t = plot_timeseries_multichannel(dats[:, 3*8192:end], 8192, chanLabels=bdfInfo["chanLabels"])
-file(t, "Eg1-AllChannels.png", width=1200, height=600)
+p = plot_timeseries(s, "Cz")
 
-singleChan = convert(Array{Float64}, vec(dats[ChannelToAnalyse,8192*2:end]))
-f = plot_timeseries(singleChan, 8192, titletext=ChanName)
-file(f, "Eg1-RawData.png", width=1200, height=600)
+file(p, "Eg1-RawData.png", width=1200, height=600)
 
-epochs = proc_epochs(dats, evtTab, verbose=true)
+p = plot_timeseries(s)
+
+file(p, "Eg1-AllChannels.png", width=1200, height=800)
+
+
+###########
+#
+# Things past here still need a wrapper function
+#
+###########
+
+
+epochs = proc_epochs(s.data, s.triggers, verbose=true)
 epochs = epochs[:,3:end,:]
 
 epochs = proc_epoch_rejection(epochs)
@@ -33,13 +37,18 @@ sweeps = proc_sweeps(epochs, verbose=true, epochsPerSweep=32)
 
 meanSweeps = squeeze(mean(sweeps,2),2)
 
+ChannelToAnalyse = 52
+
 while ChannelToAnalyse <= 64
 
     if ChannelToAnalyse == 48
         ChannelToAnalyse += 1
     end
 
-    ChanName = bdfInfo["chanLabels"][ChannelToAnalyse]
+    ChanName = s.labels[ChannelToAnalyse]
+
+    println("Processing channel $ChannelToAnalyse")
+
     fResult, s, n = proc_ftest(sweeps, 40.0391, 8192, ChannelToAnalyse)
     title = "Channel $(ChanName). SNR = $(round(fResult,2)) dB"
 
