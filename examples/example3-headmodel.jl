fa42c01bafbfa673b5746e57b0f2d9fd12d58776
Robert Luke <robert.g.luke@gmail.com>
2014-06-05 17:45:31 +0200

    Head model example

--- examples/example3-headmodel.jl
+++ examples/example3-headmodel.jl
@@ -4,4 +4,4 @@ using EEGjl
 t = import_headmodel("/Users/rluke/Documents/GITs/Leadfield.jl/data/Default50Brain.srf",
                      "/Users/rluke/Documents/GITs/Leadfield.jl/data/Default50Brain.srf",
                      "/Users/rluke/Documents/GITs/Leadfield.jl/data/FEM_Default50MRI.loc",
-                     "/Users/rluke/Documents/GITs/Leadfield.jl/data/FEM_Default50MRI-Aniso3_CR80.lft")
+                     "/Users/rluke/Documents/GITs/Leadfield.jl/data/FEM_Default50MRI-Aniso3_CR80.lft", verbose=true)
