89304618e79d2b2f71be20fb2c051e9e7a076c99
Robert Luke <robert.g.luke@gmail.com>
2014-07-02 19:39:48 +0200

    Tidy module file

--- src/EEG.jl
+++ src/EEG.jl
@@ -2,7 +2,6 @@ module EEG
 
 # Functions
 export
-    read_ASSR,
     channelNames_biosemi_1020,
     proc_hp,
     remove_template,
@@ -11,9 +10,7 @@ export
     conv_bv2tal,
     conv_spm_mni2tal,
     read_dat,
-    read_sfp,
-    save_results,
-    merge_channels
+    read_sfp
 
 # Statistics
 export
@@ -28,18 +25,27 @@ export
 include("epochs.jl")
 
 # Testing ground
-export
-    response,
-    remove_channel!,
-    add_channel,
-    assr_frequency,
-    trim_ASSR,
-    write_ASSR
-    #=import_headmodel=#
 
-# Types
+# ASSR type
 export
     ASSR,
+        read_ASSR,
+        trim_ASSR,
+        remove_channel!,
+        add_channel,
+        assr_frequency,
+        save_results,
+        write_ASSR,
+            proc_hp,                # Also defined for ASSRs
+            proc_reference,
+            merge_channels,
+            extract_epochs,
+            create_sweeps
+include("type_ASSR.jl")
+
+
+# Type
+export
     Electrodes,
     Dipoles
     #=Coordinates,=#
@@ -63,12 +69,12 @@ export
     #=ASSR_spectrogram=#
     #=plot_filter_response,=#
 #=include("plot.jl")=#
+    #=import_headmodel=#
 
 include("read.jl")
 include("processing.jl")
 include("convert.jl")
 include("biosemi.jl")
-include("type_ASSR.jl")
 include("helper.jl")
 include("signal_processing.jl")
 #=include("type_Leadfield.jl")=#
