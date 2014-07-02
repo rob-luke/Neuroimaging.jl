12477e2a47463594e0d0660423c126b6cdbdaf1c
Robert Luke <robert.g.luke@gmail.com>
2014-07-02 14:51:26 +0200

    Fixed ftest test code

--- test/ftest.jl
+++ test/ftest.jl
@@ -1,4 +1,35 @@
 using EEG
 using Base.Test
 
-@test 1 == 1
+# Tollerances to known truth are quite loose now.
+# Plan to tighten them up once all functions are known to generally work
+
+fname = "./data/test.bdf"
+
+s = read_ASSR(fname, verbose=true)
+
+@test_approx_eq_eps maximum(s.data[:,1])  189.5628662109 1
+@test_approx_eq_eps minimum(s.data[:,1]) -231.6254425049 1
+
+s = proc_hp(s, cutOff=2, order=1, verbose=true, t=1)
+#TODO: Make zero phase filter correctly account for transient period
+
+@test_approx_eq_eps maximum(s.data[:,1])  189.5628662109  2
+@test_approx_eq_eps minimum(s.data[:,1]) -231.6254425049  2
+
+s = proc_reference(s, "Cz", verbose=true)
+
+s = extract_epochs(s, verbose=true)
+
+s = create_sweeps(s, epochsPerSweep=4, verbose=true)
+
+s = ftest(s, 40.0391,  verbose=true, side_freq=0.93)
+
+@test_approx_eq_eps s.processing["ftest1"][:SNRdB][2] -7.0911276992  0.5
+@test_approx_eq_eps s.processing["ftest1"][:SNRdB][4]  2.6475683453  0.5
+
+println()
+println()
+println("!! All tests passed !!")
+println()
+println()
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,6 @@
 using EEG
 using Base.Test
 
-# write your own tests here
 @test 1 == 1
 
 include("ftest.jl")
