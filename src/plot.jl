e59c49b8bab6c20e32e1fa54a9b3b3bb2ab75206
Robert Luke <robert.g.luke@gmail.com>
2014-08-04 20:16:57 +0200

    Plot filter response

--- src/plot.jl
+++ src/plot.jl
@@ -453,6 +453,39 @@ function ASSR_spectrogram(eeg::ASSR, channel::String, lower::Number, upper::Numb
 end
 
 
+#######################################
+#
+# Filter response
+#
+#######################################
+
+# Plot filter response
+function plot_filter_response(zpk_filter::Filter, fs::Integer;
+              lower::Number=1, upper::Number=30, sample_points::Int=1024)
+
+    frequencies = linspace(lower, upper, 1024)
+    h = freqz(zpk_filter, frequencies, fs)
+    #=h = freqs(zpk_filter, frequencies, fs)=#
+    magnitude_dB = 20*log10(convert(Array{Float64}, abs(h)))
+    phase_response = (360/(2*pi))*unwrap(convert(Array{Float64}, angle(h)))
+
+    mag_plot = FramedPlot(
+         title="Filter Response",
+         ylabel="Magnitude (dB)")
+    add(mag_plot, Curve(frequencies, magnitude_dB, color="black"))
+
+    phase_plot = FramedPlot(
+         xlabel="Frequency (Hz)",
+         ylabel="Phase (degrees)")
+    add(phase_plot, Curve(frequencies, phase_response, color="black"))
+
+    t = Table(2,1)
+    t[1,1] = mag_plot
+    t[2,1] = phase_plot
+
+    return t
+end
+
 
 
 #######################################
