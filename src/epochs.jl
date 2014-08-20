cbf1f74fd5fb5f7bdcd8bb7594176ee9a0128111
Robert Luke <robert.g.luke@gmail.com>
2014-08-20 18:25:32 +0200

    Remove CI first trigger by default

--- src/epochs.jl
+++ src/epochs.jl
@@ -6,22 +6,30 @@
 #
 #######################################
 
-function extract_epochs(dats::Array, evtTab::Dict)
+function extract_epochs(dats::Array, evtTab::Dict; remove_first::Int=0)
+
 
     epochIndex = DataFrame(Code = evtTab["code"], Index = evtTab["idx"]);
     epochIndex[:Code] = epochIndex[:Code] - 252
     if findfirst(epochIndex[:Code], -4) > 0
         debug("Epochs for CI file")
         epochIndex = epochIndex[epochIndex[:Code].==-4,:]
-        epochIndex = epochIndex[2:end,:] # Often the first trigger is rubbish
+        if remove_first == 0
+            remove_first += 1
+        end
     else
         debug("Epochs for NH file")
         epochIndex = epochIndex[epochIndex[:Code].>0,:]
     end
+    epochIndex = epochIndex[remove_first+1:end,:] # Often the first trigger is rubbish
+
 
     numEpochs = size(epochIndex)[1] - 1
     lenEpochs = minimum(diff(epochIndex[:Index]))
     numChans  = size(dats)[end]
+
+    debug("Epochs = $lenEpochs x $numEpochs x $numChans")
+
     epochs = zeros(Float64, (int(lenEpochs), int(numEpochs), int(numChans)))
 
     chan = 1
