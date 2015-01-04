9a89c92bedd5096e3ebf96716d534710897fa42f
Robert Luke <robert.g.luke@gmail.com>
2015-01-04 16:26:37 +1100

    Test for sampling rate function

---
+++ test/types/SSR/SSR.jl
@@ -0,0 +1,14 @@
+using EEG
+using Base.Test
+using Logging
+
+Logging.configure(level=DEBUG)
+
+fname = joinpath(dirname(@__FILE__), "../../data", "test_Hz19.5-testing.bdf")
+
+s = read_SSR(fname)
+
+@test samplingrate(s) == 8192.0
+@test samplingrate(Int, s) == 8192
+@test isa(samplingrate(s), FloatingPoint)
+@test isa(samplingrate(Int, s), Int)
