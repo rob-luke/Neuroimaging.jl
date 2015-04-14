443fb47394e56ffcd848caf380e6d0fad46afeb4
Robert Luke <robert.luke@med.kuleuven.be>
2015-04-14 16:26:09 +0200

    Document dipole code

--- src/source_analysis/dipoles.jl
+++ src/source_analysis/dipoles.jl
@@ -1,6 +1,20 @@
 using MinMaxFilter
 
+@doc doc"""
+Dipole type.
 
+Store the location, direction and state of a dipole
+
+### Parameters
+
+* coord_system: The coordinate system that the locations are stored in
+* x,y,z: Location of dipole
+* x,y,z/ori: Orientation of dipole
+* color: Color of dipole for plotting
+* state: State of dipol
+* size: size of dipole
+
+""" ->
 type Dipole
     coord_system::String
     x::Number
@@ -17,27 +31,36 @@ end
 
 import Base.show
 function Base.show(io::IO, d::Dipole)
-
     @printf("Dipole with coordinates x=% 6.2f, y=% 6.2f, z=% 6.2f, size=% 9.5f\n", d.x, d.y, d.z, d.size)
 end
 
-
-
-
-
-
-#######################################
 #
-# Find dipoles in source data
-#
-# Use local maxima as dipoles
-#
-#######################################
+# Euclidean distance for coordinates and dipoles
+
+import Distances.euclidean
+function euclidean(a::Union(Coordinate, Dipole), b::Union(Coordinate, Dipole))
+    euclidean([a.x, a.y, a.z], [b.x, b.y, b.z])
+end
 
 
-function find_dipoles{T <: Number}(s::Array{T, 3}; window::Array{Int}=[6,6,6],
-                      x=1:size(s,1), y=1:size(s,2),
-                      z=1:size(s,3), t=1:size(s,4))
+@doc doc"""
+Find all dipole in an activity map.
+
+Determines the local maxima in a 3 dimensional array
+
+### Input
+
+* s: Activity in 3d matrix
+* window: Windowing to use in each dimension for min max filter
+* x,y,z: Coordinates associated with s matrix
+
+### Output
+
+* dips: An array of dipoles
+
+""" ->
+function find_dipoles{T <: Number}(s::Array{T, 3}; window::Array{Int}=[6,6,6], x=1:size(s,1), y=1:size(s,2),
+                                   z=1:size(s,3))
 
     info("3d dipole finding")
 
@@ -51,20 +74,20 @@ function find_dipoles{T <: Number}(s::Array{T, 3}; window::Array{Int}=[6,6,6],
     peaks = maxval[matching]
     peaks = peaks[peaks .>= 0.1 * maximum(peaks)]
 
+    # Store dipoles in an array
     dips = Array(Dipole, (1,length(peaks)))
+
     for l = 1:length(peaks)
+
         xidx, yidx, zidx = ind2sub(size(s), find(s .== peaks[l]))
-
-        dips[l] = Dipole("Unknown", x[xidx[1]], y[yidx[1]], z[zidx[1]],
-                         0, 0, 0, 0, 0,
-                         peaks[l])
-
+        dips[l] = Dipole("Unknown", x[xidx[1]], y[yidx[1]], z[zidx[1]], 0, 0, 0, 0, 0, peaks[l])
     end
 
     return dips
 end
 
 
+# 4 d version. Not used
 function find_dipoles{T <: Number}(s::Array{T, 4}; window::Array{Int}=[6,6,6,20],
                       x=1:size(s,1), y=1:size(s,2),
                       z=1:size(s,3), t=1:size(s,4))
@@ -102,21 +125,22 @@ end
 #######################################
 
 
-#
-# Euclidean distance for coordinates and dipoles
-#
+@doc doc"""
+Find best dipole relative to reference location.
 
-import Distances.euclidean
-function euclidean(a::Union(Coordinate, Dipole), b::Union(Coordinate, Dipole))
-    euclidean([a.x, a.y, a.z], [b.x, b.y, b.z])
-end
+Finds the largest dipole within a specified distance of a reference location
 
+### Input
 
-#
-# Determine the best dipole
-# Takes the largest sized dipole within set distance from reference coordinates
-#
+* ref: Reference coordinate or dipole
+* dips: Dipoles to find the best dipole from
+* maxdist: Maximum distance a dipole can be from the reference
 
+### Output
+
+* dip: The best dipole
+
+""" ->
 function best_dipole(ref::Union(Coordinate, Dipole), dips::Array{Dipole}; maxdist::Number=30)
 
     info("Calculating best dipole for $(length(dips)) dipoles")
@@ -131,16 +155,19 @@ function best_dipole(ref::Union(Coordinate, Dipole), dips::Array{Dipole}; maxdis
         bestdip = maximum(sizes[valid_dist])
         dip = dips[find(sizes .== bestdip)]
         debug("$(sum(valid_dist)) dipoles within $(maxdist)mm. ")
+
     elseif sum(valid_dist) == 1
         # Return the one valid dipole
         dip = dips[find(valid_dist)]
         debug("Only one dipole within $(maxdist)mm. ")
+
     else
         # No dipoles within distance
         # Take the closest
         bestdip = minimum(dists)
         dip = dips[find(dists .== bestdip)]
         debug("No dipole within $(maxdist)mm. ")
+        
     end
     debug("Best = $(euclidean(ref, dip[1]))")
 
@@ -151,11 +178,13 @@ end
 
 #######################################
 #
-# Optimise dipole orientation
+# Currently unused functions
 #
 #######################################
 
 function orient_dipole(dipole_data::Array{FloatingPoint, 2}, triggers, fs::Number, modulation_frequency)
+
+    warning("This function is not used. Check the output carefully")
 
     #
     # Input:  Signal projected on to orthogonal orientations and necessary parameters
@@ -188,13 +217,9 @@ function orient_dipole(dipole_data::Array{Float64, 2}, triggers, fs, modulation_
 end
 
 
-#######################################
-#
-# Best dipole orientation
-#
-#######################################
-
 function best_ftest_dipole(dipole_data::Array{FloatingPoint, 2}, triggers, fs::Number, modulation_frequency)
+
+    warning("This function is not used. Check the output carefully")
 
     #
     # Input:  Signal projected on to orthogonal orientations and necessary parameters
