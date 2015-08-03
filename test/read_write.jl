6f9307562e78f30bb01a3cebdf836e1c81208d75
Robert Luke <robert.g.luke@gmail.com>
2015-08-03 13:45:35 +0200

    Add additional tests for 4d dat

--- test/read_write.jl
+++ test/read_write.jl
@@ -1,3 +1,10 @@
+using EEG
+using Base.Test
+using Logging
+using MAT, BDF
+using SIUnits, SIUnits.ShortUnits
+
+
 #
 # BIOSEMI
 #
@@ -76,6 +83,24 @@ fname = joinpath(dirname(@__FILE__), "data", "test-4d.dat")
 sname = joinpath(dirname(@__FILE__), "data", "same.dat")
 
 x, y, z, s, t = read_dat(fname)
+
+@test size(x) == (30,)
+@test size(y) == (36,)
+@test size(z) == (28,)
+@test size(s) == (30,36,28,2)
+@test size(t) == (2,)
+
+@test maximum(x) == 72.5
+@test maximum(y) == 71.220001
+@test maximum(z) == 76.809998
+@test maximum(s) == 0.067409396
+@test maximum(t) == 0.24
+
+@test minimum(x) == -72.5
+@test minimum(y) == -103.779999
+@test minimum(z) == -58.189999
+@test minimum(s) == 0.0
+@test minimum(t) == 0.12
 
 write_dat(sname, x, y, z, s[:,:,:,:], t)
 
