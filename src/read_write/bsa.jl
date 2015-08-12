0410d7750164043c1035d1332fd6953f12392145
Robert Luke <robert.g.luke@gmail.com>
2015-08-12 17:33:17 +0200

    Fix bsa reading bug. Add test and example data

--- src/read_write/bsa.jl
+++ src/read_write/bsa.jl
@@ -22,14 +22,8 @@ function read_bsa(fname::String)
     # Read version
     first_line        = readline(file)
     separator         = search(first_line, '|')
-    version           = version_line[1:separator-1]
-    coordinate_system = version_line[separator+1:end-1]
-
-    # Create an empty dipole
-    bsa = Dipole(version, coordinate_system,
-                    Float64[], Float64[], Float64[], # X, Y, Z
-                    Float64[], Float64[], Float64[], # Xori, Yori, Zori
-                    Float64[], Float64[], Float64[]) # Color, State, Size
+    version           = first_line[1:separator-1]
+    coordinate_system = first_line[separator+1:end-1]
 
     # Read title line
     regexp = r"(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)"
@@ -38,20 +32,19 @@ function read_bsa(fname::String)
     # Read useless line
     readline(file)
 
-    # Read remaining dipoles
+    # Read dipoles
+    dips = Dipole[]
     while !eof(file)
-        d = readline(file)
-        dm = match(regexp, d)
+        dm = match(regexp, readline(file))
 
-        push!(bsa.x,     float(dm.captures[2]))
-        push!(bsa.y,     float(dm.captures[3]))
-        push!(bsa.z,     float(dm.captures[4]))
-        push!(bsa.xori,  float(dm.captures[5]))
-        push!(bsa.yori,  float(dm.captures[6]))
-        push!(bsa.zori,  float(dm.captures[7]))
-        push!(bsa.color, float(dm.captures[8]))
-        push!(bsa.state, float(dm.captures[9]))
-        push!(bsa.size,  float(dm.captures[10]))
+        println(coordinate_system)
+        println(float(dm.captures[2]))
+
+        dip = Dipole(coordinate_system, float(dm.captures[2]), float(dm.captures[3]), float(dm.captures[4]),
+                                        float(dm.captures[5]), float(dm.captures[6]), float(dm.captures[7]),
+                                        float(dm.captures[8]), float(dm.captures[9]), float(dm.captures[10]))
+
+        push!(dips, dip)
     end
 
     # Close file
@@ -59,7 +52,7 @@ function read_bsa(fname::String)
 
     debug("Version = $version")
     debug("Coordinate System  = $coordinate_system")
-    debug("Dipoles = $(length(bsa.x))")
+    debug("Dipoles = $(length(dips))")
 
-    return bsa
+    return dips
 end
