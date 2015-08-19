dd5a50895f4ca3dd3c7a59fe51c1f849bf25c9ba
Robert Luke <robert.g.luke@gmail.com>
2015-08-19 22:06:00 +0200

    Fix downsample bug. Removed lowpass filter compensation
    Low pass filter compensation is bugged

---
+++ test/types/SSR/Preprocessing.jl
@@ -0,0 +1,19 @@
+fname = joinpath(dirname(@__FILE__), "../../data", "test_Hz19.5-testing.bdf")
+
+s = read_SSR(fname)
+s = highpass_filter(s)
+s = rereference(s, "Cz")
+    keep_channel!(s, "40Hz_SWN_70dB_R")
+s = extract_epochs(s)
+s = bootstrap(s)
+
+s = lowpass_filter(s, cutOff = 200)
+s = downsample(s, 1//8)
+s = extract_epochs(s)
+s = bootstrap(s)
+
+@test_approx_eq_eps s.processing["statistics"][:AnalysisFrequency][1] s.processing["statistics"][:AnalysisFrequency][2] 0.5
+@test_approx_eq_eps s.processing["statistics"][:SNRdB][1] s.processing["statistics"][:SNRdB][2] 0.2
+@test_approx_eq_eps s.processing["statistics"][:SignalAmplitude][1] s.processing["statistics"][:SignalAmplitude][2] 0.1
+@test_approx_eq_eps s.processing["statistics"][:NoiseAmplitude][1] s.processing["statistics"][:NoiseAmplitude][2] 0.1
+
