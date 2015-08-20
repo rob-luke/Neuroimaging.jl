8ca8891fb1b951c0e88cfce51714a79da0cfafbd
Robert Luke <codles@users.noreply.github.com>
2015-08-20 15:44:15 +0200

    Plot dipoles in correct units and allow passing arrays of dipoles

--- src/types/Dipole/Plotting.jl
+++ src/types/Dipole/Plotting.jl
@@ -1,5 +1,13 @@
+function EEG.oplot(existing_plot::Table, dip::Dipole; plotting_units = 1 / (Milli * Meter), kwargs...)
+            
+    oplot_dipoles(existing_plot, dip.x * plotting_units, dip.y * plotting_units, dip.z * plotting_units; kwargs...)
+end
 
-function oplot(existing_plot::Table, dip::Dipole; plotting_units = Milli * Meter, kwargs...)
-
-    oplot_dipoles(existing_plot, dip.x / plotting_units, dip.y / plotting_units, dip.z / plotting_units; kwargs...)
+function EEG.oplot(existing_plot::Table, dips::Array{Dipole}; kwargs...)
+            
+    for dip in dips
+        existing_plot = oplot(existing_plot, dip; kwargs...)
+    end
+    
+    return existing_plot
 end
