601fc1f35d26ea18b7efc454f5212124ed74458a
Robert Luke <robert.g.luke@gmail.com>
2015-11-02 22:31:52 +0100

    Automatically run all tests
    Issue #48

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,32 +1,19 @@
-
 using EEG
 using Base.Test
 using Logging
 using MAT, BDF
 using SIUnits, SIUnits.ShortUnits
+using FileFind
 
 Logging.configure(level=DEBUG)
 
-include("miscellaneous/helper.jl")
-include("plotting/ftest.jl")
-include("preprocessing/data_rejection.jl")
-include("preprocessing/filtering.jl")
-include("read_write/read_write.jl")
-include("read_write/bsa.jl")
-include("reshaping/epochs.jl")
-include("reshaping/sweeps.jl")
-include("source_analysis/beamformers.jl")
-include("source_analysis/dipoles.jl")
-include("source_analysis/projection.jl")
-include("convert.jl")
-include("statistics/ftest.jl")
-include("statistics/gfp.jl")
-include("types/SSR/SSR.jl")
-include("types/SSR/Statistics.jl")
-include("types/SSR/Reshaping.jl")
-include("types/SSR/Preprocessing.jl")
-include("types/VolumeImage/VolumeImage.jl")
-include("types/VolumeImage/Dipoles.jl")
-include("types/VolumeImage/Plotting.jl")
-include("types/VolumeImage/Operations.jl")
-include("types/Coordinates/Coordinates.jl")
+tests = AbstractString[]
+function add_test(fname)
+    global tests
+    push!(tests, fname)
+end
+FileFind.find(".", add_test)
+
+for t in tests
+    include(t)
+end
