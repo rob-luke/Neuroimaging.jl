fe56c0c89371e2a266977db8d3188d615a07112a
Robert Luke <codles@users.noreply.github.com>
2015-11-04 11:30:54 +0100

    Update winston.jl

--- src/plotting/winston.jl
+++ src/plotting/winston.jl
@@ -25,7 +25,7 @@ Plot a 3d image file from three views (back, side and top).
 * plot_negative: should negative values be plotted
 
 """ ->
-function plot_dat{T <: AbstractFloat}(x::Array{T, 1}, y::Array{T, 1}, z::Array{T, 1}, dat_data::Array{T, 3};
+function plot_dat{T <: AbstractFloat}(x::Array{T, 1}, y::Array{T, 1}, z::Array{T, 1}, dat_data::Array{Float64, 3};
                 ncols::Int=3, max_size::Number=1, min_size=0.2,
                 min_plot_lim::Number = unique(sort(abs(vec(dat_data))))[2], threshold::Number = -Inf,
                 colorbar::Bool=false, colorbar_title::AbstractString="",
