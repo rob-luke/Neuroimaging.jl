7460574d6da65bc98267557453abce30e6b0c4dd
Robert Luke <robert.g.luke@gmail.com>
2015-11-05 14:28:25 +0100

    Ensure test directory structure is same as src
    Added empty test files to be filled

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,11 @@ using FileFind
 
 Logging.configure(level=DEBUG)
 
+
+#
+# Run all tests
+#
+
 tests = AbstractString[]
 function add_test(fname)
     global tests
@@ -21,3 +26,28 @@ FileFind.find(".", add_test)
 for t in tests
     include(t)
 end
+
+
+#
+# Check /test file structure matches /src
+#
+
+missing = AbstractString[]
+function match_tests(fname)
+    global missing
+
+    excluded = ["src/EEG.jl", "src/create_docs.jl", "runtests.jl"]
+
+    if endswith(fname, ".jl")
+        if !contains(fname, excluded[1]) & !contains(fname, excluded[2]) & !contains(fname, excluded[3])
+            matched = replace(fname, "/src/", "/test/")
+            if !isfile(matched)
+                push!(missing, matched)
+            end
+        end
+    end
+end
+
+FileFind.find("/Users/rluke/.julia/v0.4/EEG/src/", match_tests)
+
+@test length(missing) <= 1
