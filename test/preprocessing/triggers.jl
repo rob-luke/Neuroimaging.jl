e6213ce3cb1055ea486ecdb89a4c6709770c9f86
Robert Luke <robert.g.luke@gmail.com>
2015-11-14 20:44:58 +0100

    Additional trigger tests

--- test/preprocessing/triggers.jl
+++ test/preprocessing/triggers.jl
@@ -20,11 +20,28 @@ delete!(s3.triggers, "Duration")
 
 @test_throws KeyError validate_triggers(s3.triggers)
 
-s.triggers["test"] = 1
+s2 = deepcopy(s)
+s2.triggers["test"] = 1
+validate_triggers(s2.triggers)
 
-validate_triggers(s.triggers)
+s2 = deepcopy(s)
+s2.triggers["Duration"] = s2.triggers["Duration"][1:4]
+@test_throws KeyError validate_triggers(s2.triggers)
 
-s.triggers["Duration"] = s.triggers["Duration"][1:4]
-s.triggers["Code"] = s.triggers["Code"][1:4]
+s2 = deepcopy(s)
+s2.triggers["Code"] = s2.triggers["Code"][1:4]
+@test_throws KeyError validate_triggers(s2.triggers)
 
-@test_throws KeyError validate_triggers(s.triggers)
+s = read_SSR(fname, valid_triggers = [8])
+
+s = read_SSR(fname, min_epoch_length = 8388)
+
+s = read_SSR(fname, max_epoch_length = 8389)
+
+s = read_SSR(fname)
+original_length = length(s.triggers["Index"])
+s = read_SSR(fname, remove_first = 10)
+@test original_length - 10 == length(s.triggers["Index"])
+
+s = read_SSR(fname, max_epochs = 12)
+@test length(s.triggers["Index"]) == 12
