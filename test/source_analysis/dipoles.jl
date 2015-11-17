51bdddc6e27e4594e987b83e60a0206a0e1581d2
Robert Luke <robert.g.luke@gmail.com>
2015-11-17 11:21:31 +0100

    Test finding best dipole

--- test/source_analysis/dipoles.jl
+++ test/source_analysis/dipoles.jl
@@ -18,6 +18,17 @@ a = add_channel(a, best_ftest_dipole(a.data[:,1:3], a.triggers,  int(a.samplingr
 # Test dipole finding
 #
 
+fname = joinpath(dirname(@__FILE__), "../data", "test-3d.dat")
+
+t = read_VolumeImage(fname)
+dips = find_dipoles(t)
+
+left   = Talairach(-41.81/1000, -23.69/1000, 10.37/1000)   # Average literature location
+right  = Talairach( 41.81/1000, -23.69/1000, 10.37/1000)   # Average literature location
+b_dip = best_dipole(left, dips)
+
+@test euclidean(b_dip, left) == 0.023498038880581808
+@test euclidean(b_dip, left) < euclidean(b_dip, right)
 
 
 println()
