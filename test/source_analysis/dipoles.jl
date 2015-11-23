009a16bfc4e6b98e64dc1ea132482118ae831b13
Robert Luke <robert.g.luke@gmail.com>
2015-11-23 22:39:56 +0100

    Test 4d dipole finding

--- test/source_analysis/dipoles.jl
+++ test/source_analysis/dipoles.jl
@@ -31,6 +31,22 @@ b_dip = best_dipole(left, dips)
 @test euclidean(b_dip, left) < euclidean(b_dip, right)
 
 
+fname = joinpath(dirname(@__FILE__), "../data", "test-4d.dat")
+
+t = read_VolumeImage(fname)
+
+tmp = zeros(size(t.data, 1), size(t.data, 2), size(t.data, 3), 4*size(t.data, 4))
+tmp[:, :, :, 1:2] = t.data
+tmp[:, :, :, 3:4] = t.data
+tmp[:, :, :, 5:6] = t.data
+tmp[:, :, :, 7:8] = t.data
+t.data = tmp
+
+dips = find_dipoles(t.data, window=[3, 3, 3, 2])
+
+@test typeof(dips) == Array{EEG.Dipole, 1}
+@test length(dips) == 39
+
 println()
 println("!! Dipole test passed !!")
 println()
