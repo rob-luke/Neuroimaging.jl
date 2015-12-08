386da530a2c0af08f31d36852e73b55353d440a0
Robert Luke <robert.g.luke@gmail.com>
2015-12-08 18:19:35 +0100

    Test sensor matching

--- test/types/Sensors/Sensors.jl
+++ test/types/Sensors/Sensors.jl
@@ -20,6 +20,18 @@ a, b = match_sensors(s, ["Fp1", "Fp2"])
 # Test lead field sensor matching
 #
 
+L = ones(4, 3, 5)
+L[:, :, 2] = L[:, :, 2] * 2
+L[:, :, 3] = L[:, :, 3] * 3
+L[:, :, 4] = L[:, :, 4] * 4
+L[:, :, 5] = L[:, :, 5] * 5
+
+L, idx = match_sensors(L, ["meh", "Cz", "Fp1", "Fp2", "eh"], ["Fp2", "Cz"])
+
+@test size(L) == (4, 3, 2)
+@test L[:, :, 1] == ones(4, 3) * 4
+@test L[:, :, 2] == ones(4, 3) * 2
+@test idx == [4, 2]
 
 
 #
