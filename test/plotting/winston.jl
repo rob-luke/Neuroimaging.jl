7ad5293800d5d5470e80140dbe115e2115933d41
Robert Luke <robert.g.luke@gmail.com>
2015-12-10 20:56:59 +0100

    Extract sweeps before plotting spectrum

--- test/plotting/winston.jl
+++ test/plotting/winston.jl
@@ -20,4 +20,7 @@ p = SSR_spectrogram(s, "20Hz_SWN_70dB_R", 1.0, 50.0, seconds = 2)
 # Plot spectrum
 #
 
+s = extract_epochs(s)
+s = create_sweeps(s, epochsPerSweep = 2)
+
 p = plot_spectrum(s, "20Hz_SWN_70dB_R")
