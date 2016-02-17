d97f93084321558551081f771ec119108e733705
Robert Luke <robert.g.luke@gmail.com>
2016-02-17 13:48:34 +0100

    Find largest dipole even if on edge of source space

--- src/source_analysis/dipoles.jl
+++ src/source_analysis/dipoles.jl
@@ -37,6 +37,15 @@ function find_dipoles{T <: Number}(s::Array{T, 3}; window::Array{Int}=[6,6,6], x
         push!(dips, Dipole("Unknown", x[xidx[1]], y[yidx[1]], z[zidx[1]], 0, 0, 0, 0, 0, peaks[l]))
     end
 
+    left_side = s[x .< 0, :, :]
+    xidx, yidx, zidx = ind2sub(size(left_side), find(left_side .== maximum(left_side)))
+    push!(dips, Dipole("Unknown", x[xidx[1]], y[yidx[1]], z[zidx[1]], 0, 0, 0, 0, 0, maximum(left_side)))
+
+    right_side = s[x .> 0, :, :]
+    xidx, yidx, zidx = ind2sub(size(right_side), find(right_side .== maximum(right_side)))
+    x_tmp = x[x .> 0]
+    push!(dips, Dipole("Unknown", x_tmp[xidx[1]], y[yidx[1]], z[zidx[1]], 0, 0, 0, 0, 0, maximum(right_side)))
+
     # Sort dipoles by size
     vec(dips[sortperm([dip.size for dip in dips], rev=true)])
 end
@@ -93,37 +102,45 @@ Finds the largest dipole within a specified distance of a reference location
 * dip: The best dipole
 
 """ ->
-function best_dipole(ref::Union{Coordinate, Dipole}, dips::Array{Dipole}; maxdist::Number=0.30)
+function best_dipole(ref::Union{Coordinate, Dipole}, dips::Array{Dipole}; maxdist::Number=0.30, min_dipole_size::Real=-Inf, kwargs...)
 
     Logging.info("Calculating best dipole for $(length(dips)) dipoles")
 
-    # Find all dipoles within distance
-    dists = [euclidean(ref, dip) for dip=dips]
-    valid_dist = dists .< maxdist
+    dips = dips[find([d.size > min_dipole_size for d in dips])]
 
-    if sum(valid_dist) >= 2
-        # Valid dipoles exist find the largest one
-        sizes = [dip.size for dip =dips]
-        bestdip = maximum(sizes[valid_dist])
-        dip = dips[find(sizes .== bestdip)]
-        debug("$(sum(valid_dist)) dipoles within $(maxdist)mm. ")
+    if length(dips) > 0
 
-    elseif sum(valid_dist) == 1
-        # Return the one valid dipole
-        dip = dips[find(valid_dist)]
-        debug("Only one dipole within $(maxdist)mm. ")
+      # Find all dipoles within distance
+      dists = [euclidean(ref, dip) for dip=dips]
+      valid_dist = dists .< maxdist
+
+      if sum(valid_dist) >= 2
+          # Valid dipoles exist find the largest one
+          sizes = [dip.size for dip =dips]
+          bestdip = maximum(sizes[valid_dist])
+          dip = dips[find(sizes .== bestdip)]
+          debug("$(sum(valid_dist)) dipoles within $(maxdist)mm. ")
+
+      elseif sum(valid_dist) == 1
+          # Return the one valid dipole
+          dip = dips[find(valid_dist)]
+          debug("Only one dipole within $(maxdist)mm. ")
+
+      else
+          # No dipoles within distance
+          # Take the closest
+          bestdip = minimum(dists)
+          dip = dips[find(dists .== bestdip)]
+          debug("No dipole within $(maxdist)mm. ")
+
+      end
+      debug("Best = $(euclidean(ref, dip[1]))")
+
+      return dip[1]
 
     else
-        # No dipoles within distance
-        # Take the closest
-        bestdip = minimum(dists)
-        dip = dips[find(dists .== bestdip)]
-        debug("No dipole within $(maxdist)mm. ")
-
+      return NaN
     end
-    debug("Best = $(euclidean(ref, dip[1]))")
-
-    return dip[1]
 end
 
 
@@ -201,5 +218,4 @@ end
 function best_ftest_dipole(dipole_data::Array{Float64, 2}, triggers, fs, modulation_frequency)
     best_ftest_dipole(convert(Array{AbstractFloat, 2}, dipole_data), triggers, fs, modulation_frequency)
 end
-
 
