302bf64853e62f9eab0149b840e43bb6503f2adb
Robert Luke <robert.luke@med.kuleuven.be>
2016-02-19 15:46:08 +0100

    Only show legend on last dipole of array

--- src/types/Dipole/Plotting.jl
+++ src/types/Dipole/Plotting.jl
@@ -8,12 +8,15 @@ function EEG.plot(p, dip::Union{Dipole, Coordinate}; c=:green, m=(8, :rect), l="
 end
 
 
-function EEG.plot(p, dips::Union{Array{Dipole}, Array{Coordinate}}; kwargs...)
+function EEG.plot(p, dips::Union{Array{Dipole}, Array{Coordinate}}; l = "", kwargs...)
 
-  for dip in dips
-    p = EEG.plot(p, dip; kwargs...)
-  end
+    for dip in 1:length(dips)
+        if dip == length(dips)
+            p = EEG.plot(p, dips[dip], l = l; kwargs...)
+        else
+            p = EEG.plot(p, dips[dip], l = ""; kwargs...)
+        end
+    end
 
-  return p
+    return p
 end
-
