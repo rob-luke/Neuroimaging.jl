1b7458fa17f191610e3f5d7b27f48f8cd6181bd5
Robert Luke <robert.g.luke@gmail.com>
2016-04-30 22:51:09 +0200

    Add reference tests

--- test/preprocessing/reference.jl
+++ test/preprocessing/reference.jl
@@ -1,4 +1,7 @@
 facts("Referencing") do
+
+    signals = [0 1 2] .* ones(5, 3)
+
     context("Remove template") do
 
 	signals = [0 1 2] .* ones(5, 3)
@@ -8,8 +11,16 @@ facts("Referencing") do
 
     end
     context("Reference to channel") do
+
+	@fact rereference(signals, 3) --> [-2 -1 0] .* ones(5, 3)
+	@fact rereference(signals, "C2", ["C1", "C2", "C3"]) --> [-1 0 1] .* ones(5, 3)
+
     end
 
     context("Reference to group of channels") do
+
+	@fact rereference(signals, [1, 2, 3]) --> [-1 0 1] .* ones(5, 3)
+	@fact rereference(signals, ["C2", "C1", "C3"], ["C1", "C2", "C3"]) --> [-1 0 1] .* ones(5, 3)
+
     end
 end
