4775f453cae1b13f0a27d10f1225525467a7cdc8
Robert Luke <robert.g.luke@gmail.com>
2016-05-01 21:34:05 +0200

    Finalise VolumeImages test move to FactCheck.jl

--- test/types/VolumeImage/VolumeImage.jl
+++ test/types/VolumeImage/VolumeImage.jl
@@ -1,5 +1,5 @@
 facts("Volume Image") do
-    fname = joinpath(dirname(@__FILE__), "../../data", "test-4d.dat")
+    fname = joinpath(dirname(@__FILE__), "../../data", "test-3d.dat")
     t = read_VolumeImage(fname)
     t2 = read_VolumeImage(fname)
 
@@ -25,13 +25,71 @@ facts("Volume Image") do
 	t2.units = "A/m^3"
 	@fact_throws t + t2
 
+
     end
+
+    context("Min/Max") do
+
+	@fact maximum(t) --> 33.2692985535
+	@fact minimum(t) --> -7.5189352036
+
+	b = t * 2
+
+	@fact b.data[1, 1, 1] --> 2 * b.data[1, 1, 1]
+	@fact b.data[2, 2, 2] --> 2 * b.data[2, 2, 2]
+
+	c = mean([b, t])
+
+	@fact size(c.data) --> size(b.data)
+	@fact c.data[1, 1, 1] --> (b.data[1, 1, 1] + t.data[1, 1, 1]) / 2
+
+	@fact maximum([b, t]) --> maximum(t) * 2
+	@fact minimum([b, t]) --> minimum(b)
+
+	n = normalise([b, t])
+
+	@fact maximum(n) --> less_than_or_equal(1.0)
+	@fact minimum(n) --> greater_than_or_equal(-1.0)
+
+    end
+
+
+    context("Dimension checks") do
+	t2 = deepcopy(t); t2.x = t2.x[1:3]
+	@fact_throws KeyError EEG.dimensions_equal(t, t2)
+	t2 = deepcopy(t); t2.y = t2.y[1:3]
+	@fact_throws KeyError EEG.dimensions_equal(t, t2)
+	t2 = deepcopy(t); t2.z = t2.z[1:3]
+	@fact_throws KeyError EEG.dimensions_equal(t, t2)
+	t2 = read_VolumeImage(joinpath(dirname(@__FILE__), "../../data", "test-4d.dat"))
+	@fact_throws KeyError EEG.dimensions_equal(t, t2)
+    end
+
 
     context("Average") do
 	s = mean(t)
     end
 
+    context("Find dipoles") do
+
+	t = read_VolumeImage(fname)
+	dips = find_dipoles(mean(t))
+	@fact size(dips) --> (11,)
+
+	fname = joinpath(dirname(@__FILE__), "../../data", "test-4d.dat")
+	t2 = read_VolumeImage(fname)
+	dips = find_dipoles(mean(t2))
+	@fact size(dips) --> (5,)
+
+	# Test dipoles are returned in order of size
+	@fact issorted([dip.size for dip in dips], rev = true) --> true
+    end
+
+
     context("Show") do
+	show(t)
+	show(normalise(t))
+	t.info["Regularisation"] = 1.2
 	show(t)
     end
 
