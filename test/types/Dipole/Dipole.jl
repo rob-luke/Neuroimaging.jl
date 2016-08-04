0b5bdb1021152796a356edeab189e84f1680e49f
Robert Luke <robert.luke@med.kuleuven.be>
2016-08-04 14:09:04 +0200

    Test taking mean of dipoles

--- test/types/Dipole/Dipole.jl
+++ test/types/Dipole/Dipole.jl
@@ -4,16 +4,27 @@ facts("Dipoles") do
 
     context("Create") do
 
-        dip1 = Dipole("Talairach", 1, 2, 3, 0, 0, 0, 1, 1, 1)
+        dip1 = Dipole("Talairach", 1, 2, 1, 0, 0, 0, 1, 1, 1)
         dip2 = Dipole("Talairach", 1, 2, 3, 0, 0, 0, 2, 2, 2)
 
         dips = push!(dips, dip1)
         dips = push!(dips, dip2)
+
+        @fact length(dips) --> 2
     end
 
     context("Show") do
 
         show(dips[1])
         show(dips)
+    end
+
+
+    context("Mean") do
+
+        b = mean(dips)
+        @fact b.x --> 1.0 * SIUnits.ShortUnits.m
+        @fact b.y --> 2.0 * SIUnits.ShortUnits.m
+        @fact b.z --> 2.0 * SIUnits.ShortUnits.m
     end
 end
