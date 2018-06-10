d5fc45a933221a0815a0b58b0f9620f137ab485f
Robert Luke <hello@robertluke.net>
2018-06-10 13:07:16 +1000

    Modify elp reader to use regex rathen than external package

--- src/read_write/elp.jl
+++ src/read_write/elp.jl
@@ -5,7 +5,7 @@
 #######################################
 
 @doc """
-Read elp file
+Read elp file containing sensor locations
 
 
 #### Input
@@ -17,28 +17,32 @@ Read elp file
 * `elecs`: Array of electrode objects
 """ ->
 function read_elp(fname::AbstractString; coordinate=Talairach, r::Real=90)
-    # This does not work yet, need to convert to 3d coord system
 
     info("Reading elp file = $fname")
 
     # Create an empty electrode set
     elecs = Electrode[]
 
-    # Read file
-    df = readtable(fname, header = false, separator = ' ')
-
-    phi = df[:x2]
-    theta = df[:x3]
-
-    # Save locations
-    x = r .* sin.(phi*(pi/180)) .* cos.(theta*(pi/180))
-    y = r .* sin.(phi*(pi/180)) .* sin.(theta*(pi/180)) - 17.5
-    z = r .* cos.(phi*(pi/180))
+    # Read file and match to expected file format
+    file = readstring(fname)
+    regexp = r"(\S+)\s+(\S+)\s+(\S+)"
+    m = matchall(regexp, file)
 
     # Convert label to ascii and remove '
-    labels = df[:x1]
-    for i = 1:length(labels)
-        push!(elecs, Electrode(replace(labels[i], "'", "" ), coordinate(x[i], y[i], z[i]), Dict()))
+    for idx = 1:length(m)
+
+        local_matches = match(regexp, m[idx])
+
+        # Extract phi and theta
+        phi = float(local_matches[2])
+        theta = float(local_matches[3])
+
+        # Convert to x, y, z
+        x = r .* sin.(phi*(pi/180)) .* cos.(theta*(pi/180))
+        y = r .* sin.(phi*(pi/180)) .* sin.(theta*(pi/180)) - 17.5
+        z = r .* cos.(phi*(pi/180))
+
+        push!(elecs, Electrode(replace(local_matches[1], "'", "" ), coordinate(x, y, z), Dict()))
     end
 
     debug("Imported $(length(elecs)) electrodes")
