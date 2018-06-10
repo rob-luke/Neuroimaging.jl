6dda1ffcd087dbcd78652532a26d45b4271b8178
Robert Luke <hello@robertluke.net>
2018-06-10 11:51:56 +1000

    Modify sfp reader to use regex rather than external package

--- src/read_write/sfp.jl
+++ src/read_write/sfp.jl
@@ -5,32 +5,30 @@
 #######################################
 
 @doc """
-Read sfp file
+Read sfp file containing sensor locations
 
 #### Input
-* `fname`: Name or path for the sfp file
+* `fname`: Path for the sfp file
 
 #### Output
 * `elec`: Electrodes object
 """ ->
 function read_sfp(fname::AbstractString; coordinate=Talairach)
+
     info("Reading dat file = $fname")
 
     # Create an empty electrode set
     elecs = Electrode[]
 
-    # Read file
-    df = readtable(fname, header = false, separator = ' ')
-
-    # Save locations
-    x = df[:x2]
-    y = df[:x3]
-    z = df[:x4]
+    # Read file and match to expected file format
+    file = readstring(fname)
+    regexp = r"(\S+)\s+(\S+)\s+(\S+)\s+(\S+)"
+    m = matchall(regexp, file)
 
     # Convert label to ascii and remove '
-    labels = df[:x1]
-    for i = 1:length(labels)
-        push!(elecs, Electrode(replace(labels[i], "'", "" ), coordinate(x[i], y[i], z[i]), Dict()))
+    for idx = 1:length(m)
+        local_matches = match(regexp, m[idx])
+        push!(elecs, Electrode(replace(local_matches[1], "'", "" ), coordinate(float(local_matches[2]), float(local_matches[3]), float(local_matches[4])), Dict()))
     end
 
     debug("Imported $(length(elecs)) electrodes")
