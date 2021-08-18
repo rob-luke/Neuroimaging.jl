99c7df7d5d920b4b87e8b8246cbe9642c61ca1b5
Robert Luke <hello@robertluke.net>
2021-08-18 21:24:41 +1000

    Enable documentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,14 @@
+push!(LOAD_PATH,"../src/")
 using Documenter, EEG
 
-makedocs()
+makedocs(
+    modules = [EEG],
+    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+    sitename = "EEG.jl",
+    authors  = "Robert Luke",
+)
 
 deploydocs(
-    repo = "github.com/codles/EEG.jl.git",
-    julia = "0.5",
+    repo = "github.com/rob-luke/EEG.jl.git",
+    devbranch = "main"
 )
