3efd8f9c9a470aa99be21637200e16499bedecd5
Robert Luke <hello@robertluke.net>
2021-08-19 12:10:46 +1000

    Format

--- src/datasets/datasets.jl
+++ src/datasets/datasets.jl
@@ -1,2 +1,8 @@
-register(DataDep("BioSemiTestFiles", "Manafacturer provided example files",["https://www.biosemi.com/download/BDFtestfiles.zip"]; post_fetch_method = [file->run(`unzip $file`)]))
-
+register(
+    DataDep(
+        "BioSemiTestFiles",
+        "Manafacturer provided example files",
+        ["https://www.biosemi.com/download/BDFtestfiles.zip"];
+        post_fetch_method = [file -> run(`unzip $file`)],
+    ),
+)
