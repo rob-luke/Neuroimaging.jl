8298ebedcec86474962f90cbf462bdbadc062564
Robert Luke <hello@robertluke.net>
2021-08-19 12:45:50 +1000

    More tests

--- test/datasets/datasets.jl
+++ test/datasets/datasets.jl
@@ -1,4 +1,10 @@
+# Tests to ensure datasets are available and correct
+
+# Basic biosemi data file
 data_path = joinpath(datadep"BioSemiTestFiles", "Newtest17-2048.bdf")
 s = read_SSR(data_path)
-
-@test samplerate(s) == 200
+@test samplerate(s) == 2048
+@test length(channelnames(s)) == 16
+@test length(s.triggers["Index"])
+@test length(s.triggers["Code"])
+@test length(s.triggers["Duration"])
