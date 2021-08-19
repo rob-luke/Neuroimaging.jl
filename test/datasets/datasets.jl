14b2d2231f39eb7d8bfcc60c9d26b36a562b1598
Robert Luke <hello@robertluke.net>
2021-08-19 13:37:33 +1000

    Fixes

--- test/datasets/datasets.jl
+++ test/datasets/datasets.jl
@@ -3,7 +3,7 @@
 # Basic biosemi data file
 data_path = joinpath(datadep"BioSemiTestFiles", "Newtest17-2048.bdf")
 s = read_SSR(data_path)
-@test samplerate(s) == 2048
+@test samplingrate(s) == 2048
 @test length(channelnames(s)) == 16
 @test length(s.triggers["Index"]) == length(s.triggers["Code"])
 @test length(s.triggers["Code"]) == ength(s.triggers["Duration"])
