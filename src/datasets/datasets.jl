bfc64abb266141cb0657c222bd7a1abf570be29f
Robert Luke <hello@robertluke.net>
2021-08-19 13:12:07 +1000

    More tests

--- src/datasets/datasets.jl
+++ src/datasets/datasets.jl
@@ -1,8 +1,10 @@
-register(
-    DataDep(
-        "BioSemiTestFiles",
-        "Manafacturer provided example files",
-        ["https://www.biosemi.com/download/BDFtestfiles.zip"];
-        post_fetch_method = [file -> run(`unzip $file`)],
-    ),
-)
+function __init__()
+    register(
+        DataDep(
+            "BioSemiTestFiles",
+            "Manafacturer provided example files",
+            ["https://www.biosemi.com/download/BDFtestfiles.zip"];
+            post_fetch_method = [file -> run(`unzip $file`)],
+        ),
+    )
+end
--- test/datasets/datasets.jl
+++ test/datasets/datasets.jl
@@ -5,6 +5,5 @@ data_path = joinpath(datadep"BioSemiTestFiles", "Newtest17-2048.bdf")
 s = read_SSR(data_path)
 @test samplerate(s) == 2048
 @test length(channelnames(s)) == 16
-@test length(s.triggers["Index"])
-@test length(s.triggers["Code"])
-@test length(s.triggers["Duration"])
+@test length(s.triggers["Index"]) == length(s.triggers["Code"])
+@test length(s.triggers["Code"]) == ength(s.triggers["Duration"])
