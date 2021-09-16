00edb6f493196ccad1c1d493b178e21176efdc71
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2021-09-17 00:07:49 +0200

    added limo dataset, waiting for eeglab importer

--- src/datasets/datasets.jl
+++ src/datasets/datasets.jl
@@ -16,5 +16,13 @@ function __init__()
             ];
             post_fetch_method = [file -> run(`unzip $file`)],
         ),
+    ),
+    register(
+        DataDep(
+            "LIMO dataset subject 1",
+            "Face Experiment with continuous predictor",
+            ["https://files.de-1.osf.io/v1/resources/52rea/providers/osfstorage/5cde823c8d6e050018595862/?zip="];
+            post_fetch_method = [file -> run(`unzip $file`)],
+        )
     )
 end
