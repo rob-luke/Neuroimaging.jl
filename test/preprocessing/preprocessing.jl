ce820bbbe78fe7b60f7b3ed646815072c29b6529
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2021-09-17 22:15:38 +0200

    undo the Base.filter import

--- test/preprocessing/preprocessing.jl
+++ test/preprocessing/preprocessing.jl
@@ -69,21 +69,25 @@ end
 end
 
 @testset "Filter" begin
+    using DSP
     s = sin.(0:0.1:10*π)
-	designmethod = FIRWindow(hamming(60+1))
-    fs = 10.0u"Hz"
+	designmethod = FIRWindow(DSP.hamming(60+1))
+    fs = 10.0
 	responsetype = Highpass(2,fs=fs)
 	fobj = digitalfilter(responsetype,designmethod)
     function generate_dummyeeg(data;fs=1.0u"Hz")
         return GeneralEEG(data,[],Dict(),Dict(),fs,[],"","",Dict(),Dict()) 
     end
-    eeg = generate_dummyeeg(s,fs=fs)
+    eeg = generate_dummyeeg(s,fs=10.0u"Hz")
     
     @testset "Custom Filter" begin
-        @test all(filter(s,fobj,filtfilt=true) .<= 0.1)
-        @test all(filter(s,fobj,filtfilt=false) .<= 0.1)
-        @test all(filter(eeg,responsetype,designmethod).data .<= 0.1)
+        @test all(Neuroimaging.filter(s,fobj,filtfilt=true) .<= 0.1)
+        @test all(Neuroimaging.filter(s,fobj,filtfilt=false) .<= 0.1)
+        @test all(Neuroimaging.filter(eeg,responsetype,designmethod).data .<= 0.1)
         
     end
-
+    @testset "default window size" begin
+        filterorder = Neuroimaging.default_fir_filterorder(responsetype,samplingrate(eeg))
+        @test iseven(filterorder)
+    end
 end
